612f8da76d2c3073be0c9e8064a3f6423c266594
Apple <apple@Apples-MacBook-Pro.local>
2020-02-04 12:43:47 +0530

    Updates

---
+++ precache-manifest.805dafd4187e20b4d22cdca5b41095de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2abf65e3878b01a5301cb9fcecbb1e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "babb0c6ef0c6c0347634",
+    "url": "/robofriends/static/css/main.134fc3d8.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/robofriends/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babb0c6ef0c6c0347634",
+    "url": "/robofriends/static/js/main.eac4155a.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
